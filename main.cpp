5e59c0b5f9e2c7ad7aabd3455696cd8bbb761204
Rinat Andreev <97101993+RinatAndreev@users.noreply.github.com>
2024-03-19 14:11:37 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,55 @@
+#include <iostream>
+#include <fstream>
 #include <string>
-// You may include other headers, as needed
+using namespace std;
 
-using std::string;
 
-void countAndWriteDigitsPerLine(string filename, string outputFilename)
-{
-    // Your code here
+
+void countAndWriteDigitsPerLine(const std::string& filename, const std::string& outputFilename) {
+    ifstream inputFile(filename);
+    ofstream outputFile(outputFilename);
+
+    string line;
+    while (getline(inputFile, line)) {
+        int digitCount = 0;
+        for (char c : line) {
+            if (isdigit(c)) {
+                digitCount++;
+            }
+        }
+        outputFile << digitCount << std::endl;
+    }
+
+    inputFile.close();
+    outputFile.close();
 }
 
 unsigned int countMaxDigits(string filename)
 {
-    // Your code here
+    ifstream inputFile(filename);
+    string line;
+    unsigned int maxDigitCount = 0;
+
+    while (getline(inputFile, line)) {
+        unsigned int digitCount = 0;
+        for (char c : line) {
+            if (isdigit(c)) {
+                digitCount++;
+            }
+        }
+        maxDigitCount = max(maxDigitCount, digitCount);
+    }
+
+    inputFile.close();
+    return maxDigitCount;
+}
+
+int main() {
+    string inputFilename = "filename.txt";
+    string outputFilename = "outputfilename.txt";
+
+    countAndWriteDigitsPerLine(inputFilename, outputFilename);
+    cout << countMaxDigits(inputFilename);
+       return 0;
+
 }
